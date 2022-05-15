6b3926cdfd49867bb4bfcf214dc9214f6a17fa1a
bhavanichandra <bhavanichandra9@gmail.com>
2022-05-16 02:02:56 +0530

    changes to index.js

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 const dotenv = require('dotenv');
+dotenv.config();
 
-if (dotenv.config().error) {
-    throw new Error('Could not find .env file');
-} else {
-    const express = require('express');
-    const app = express();
-    const appController = require('./controller/app.controller');
-    app.use(express.json());
+const express = require('express');
+const app = express();
+const appController = require('./controller/app.controller');
+app.use(express.json());
+
+app.get('/ping', (req, res) => res.json({message: 'Working..!'}));
+
+app.get('/apps', appController.getApps);
+
+app.post('/apps', appController.saveApp);
+
+app.use((err, req, res, next) => {
+    res.status(500)
+        .json({message: err});
     
-    app.get('/ping', (req, res) => res.json({message: 'Working..!'}));
-    
-    app.get('/apps', appController.getApps);
-    
-    app.post('/apps', appController.saveApp);
-    
-    app.use((err, req, res, next) => {
-        res.status(500)
-            .json({message: err});
-        
-    });
-    
-    // app.listen(8080, () => {
-    //     console.log('Started Listening');
-    // });
-    // export 'app'
-    module.exports = app;
-}
+});
+
+// app.listen(8080, () => {
+//     console.log('Started Listening');
+// });
+// export 'app'
+module.exports = app;
+
